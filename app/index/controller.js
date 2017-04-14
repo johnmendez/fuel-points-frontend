61f5a19ef728bb06a8561e392ea5de786ca2e25d
Zach Edwards <tolklorist@gmail.com>
2017-04-13 23:11:47 -0500

    added join now cards and map is still there

--- app/index/controller.js
+++ app/index/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   lat: 45.519743,
   lng: -122.680522,
-  zoom: 7,
+  zoom: 6,
   actions: {
     updateCenter(e) {
       let center = e.target.getCenter();
