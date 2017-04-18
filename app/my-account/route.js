1bff0cbe581ffba287634e5716b73f21667968f0
Zach Edwards <tolklorist@gmail.com>
2017-04-18 16:29:51 -0500

    my account must be logged in

--- app/my-account/route.js
+++ app/my-account/route.js
@@ -1,16 +1,9 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend( {
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
-
-  beforeModel() {
-    if (this.get('session.isAuthenticated')) {
-      this.transitionTo('index');
-    }
-},
-
   model() {
-    return this.store.findAll('vehicles');
+    return this.store.findAll('vehicle');
   }
 });
