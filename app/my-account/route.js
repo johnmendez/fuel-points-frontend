b1e2089cdf4f397b67e03c1d2dd00b66f093d3dd
johnmendez <john.a.mendez2012@gmail.com>
2017-04-18 13:39:10 -0500

    Quick Save 1:38pm

--- app/my-account/route.js
+++ app/my-account/route.js
@@ -1,16 +1,22 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  session: Ember.inject.service(),
+export default Ember.Route.extend(AuthenticatedRouteMixin);
 
-  beforeModel() {
-    if (this.get('session.isAuthenticated')) {
-      this.transitionTo('index');
-    }
-},
 
-  model() {
-    return this.store.findAll('vehicle');
-  }
-});
+// import Ember from 'ember';
+// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+//
+// export default Ember.Route.extend(AuthenticatedRouteMixin, {
+//   session: Ember.inject.service(),
+//
+//   beforeModel() {
+//     if (this.get('session.isAuthenticated')) {
+//       this.transitionTo('index');
+//     }
+//   },
+//
+//   model() {
+//     return this.store.findAll('vehicle');
+//   }
+// });
