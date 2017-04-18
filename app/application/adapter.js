862b92e5b9bac14af014dde888f5955d9bef5934
johnmendez <john.a.mendez2012@gmail.com>
2017-04-18 14:25:05 -0500

    Quick Save 2:25pm

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 import config from 'fuel-points/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: DS.config.host,
+  host: config.DS.host,
   namespace: config.DS.namespace,
 });
