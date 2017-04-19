cf75a2c3708ce871620cedc9e4052680b35389ca
Zach Edwards <tolklorist@gmail.com>
2017-04-18 20:20:44 -0500

    fixing register style

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -4,7 +4,6 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:token',
-
   host: config.DS.host,
   namespace: config.DS.namespace,
 });
