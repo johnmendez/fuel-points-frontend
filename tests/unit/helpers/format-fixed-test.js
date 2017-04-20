e401edee0940424454b4d21ece161be45691d451
Zach Edwards <tolklorist@gmail.com>
2017-04-20 17:12:29 -0500

    up to date

---
+++ tests/unit/helpers/format-fixed-test.js
@@ -0,0 +1,12 @@
+
+import { formatFixed } from 'fuel-points/helpers/format-fixed';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format fixed');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatFixed([42]);
+  assert.ok(result);
+});
+
